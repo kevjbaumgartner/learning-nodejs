ed5fc31b75455c9bd661873618509138ea60d778
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-02-03 16:23:05 -0500

    myfirstmodule.js
    Introduction to node modules

--- myfirst/myfirst.js
+++ myfirst/myfirst.js
@@ -1,6 +1,8 @@
 var http  = require('http');
+var dt = require('../dt/myfirstmodule.js');
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('The date and time is: ' + dt.myDateTime());
 	res.end('Hello World!');
 }).listen(8080);
