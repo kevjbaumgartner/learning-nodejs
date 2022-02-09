baed1bc7b60da3b2c57d183659a57a0368646ac1
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-02-09 16:28:18 -0500

    Examples of MySQL added

--- demo_mysql/sql.js
+++ demo_mysql/sql.js
@@ -1,12 +1,28 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
 var con = mysql.createConnection({
-	host: "", //redacted
-	user: "", //redacted
-	password: "" //redacted
+	host: "localhost", 		// redacted
+	user: "root",			// redacted
+	password: "notroot",	// redacted
+	database: "demodb"		// redacted
 });
+
+// var str = "CREATE DATABASE demodb";
+// var str = "CREATE TABLE demotable (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), val VARCHAR(255))"
+// var str = "INSERT INTO demotable (name, val) VALUES ?";
+// var insertValues = [
+// 	['nameA', 'valA'],
+// 	['nameB', 'valB'],
+// 	['nameC', 'valC']
+// ];
+var str = "SELECT * FROM demotable";
 
 con.connect(function (err) {
 	if (err) throw err;
 	console.log("Connected");
+	con.query(str, function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+	});
+	con.end();
 });
